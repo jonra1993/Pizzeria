03c2b7621cf2cf7b247fe0e72cad8408a922af12
Jona <jon_ra@hotmail.es>
2018-08-02 21:57:40 -0500

    repara icono de admin

--- layouts/header.php
+++ layouts/header.php
@@ -10,13 +10,11 @@
             else echo "Sistema simple de inventario";?>
     </title>
 	
-    <!--meta name="viewport" content="width=device-width, initial-scale=1"-->
-    <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"-->
-    <!--link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" /-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script-->
     <link rel="stylesheet" href="libs/css/main.css" />
   </head>
 
