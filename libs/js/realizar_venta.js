8f171d48f8c856b1b43c8bf08f3d32c049fc2ebc
Unknown <diegonf26@gmail.com>
2018-08-08 14:53:07 -0500

    cards pizzas centradas

--- libs/js/realizar_venta.js
+++ libs/js/realizar_venta.js
@@ -4,16 +4,7 @@ var sel_pizza_sec="Pizzas";
 function selec_categ(nombre_cat) {
   if(nombre_cat=="Pizzas"){
     var f = document.getElementById("selc_pizzas_tam");
-    if(f.style.display == 'block'){
-      f.style.display = 'none';
-    }
-    else{
-      f.style.display = 'block';
-    }
-  }
-  else 
-  {
-
+    centrar(f);
   }
 }
 
@@ -21,7 +12,7 @@ function tam_pizzas(tama){
   if(tama!="Porcion"){
     var e = document.getElementById("selc_pizzas_nor_esp");
     var f = document.getElementById("selc_pizzas_tam");
-    e.style.display = 'block';
+    centrar(e);
     f.style.display = 'none';
   }
 }
@@ -29,7 +20,23 @@ function tam_pizzas(tama){
 function pizzas_normal(){
   var e = document.getElementById("selc_pizzas_tipo");
   var f = document.getElementById("selc_pizzas_nor_esp");
-  e.style.display = 'block';
+  centrar(e);
   f.style.display = 'none';
 
+}
+
+function ingre_extra(tipo_pizza){
+  var e = document.getElementById("selc_extra");
+  var f = document.getElementById("selc_pizzas_tipo");
+  centrar(e);
+  f.style.display = 'none';
+
+}
+
+function centrar(id){
+  id.style.display = 'flex';
+  id.style.paddingTop='2%';
+  id.style.alignItems='center';
+  id.style.flexWrap= 'wrap';
+  id.style.justifyContent= 'center';
 }
