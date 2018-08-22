3b15b8c75c23b30272a60df6b934edd10c3cbc9b
Jona <jon_ra@hotmail.es>
2018-08-22 14:46:32 -0500

    nota de venta y de cocina con crecimiento dinámico
    se mejoro la presentación de estas notas y se configuró para que sea dinámico

--- realizar_venta_pdf.php
+++ realizar_venta_pdf.php
@@ -6,28 +6,34 @@ include_once("fpdf/fpdf.php");
 // Better table
 function ImprovedTable($pdf,$header,$data,$subtotal)
 {
+    $pdf->setFont("Courier",null,9);
     // Column widths
     $q=4;
     $w = array(4, 48, 11, 11);
     // Header
     for($i=0;$i<count($header);$i++)
-        $pdf->Cell($w[$i],4,utf8_decode($header[$i]),1,0,"C");
+        $pdf->Cell($w[$i],4,utf8_decode($header[$i]),0,0,"L");
     $pdf->Ln();
     // Data
-    for ($x = 0; $x < (sizeof($data)/4); $x++) {
-        $pdf->Cell($w[0],4,utf8_decode($data[$x*$q]),'LR',0,"C");
-        $pdf->Cell($w[1],4,utf8_decode($data[$x*$q+1]),'LR',0,"C");
-        $pdf->Cell($w[2],4,utf8_decode($data[$x*$q+2]),'LR',0,"C");
-        $pdf->Cell($w[3],4,utf8_decode($data[$x*$q+3]),'LR',0,"C");
+    $k=(sizeof($data)/4);
+    for ($x = 0; $x < $k; $x++) {
+        $pdf->Cell($w[0],4,utf8_decode($data[$x*$q]),0,0,"L");
+        $pdf->Cell($w[1],4,utf8_decode($data[$x*$q+1]),0,0,"L");
+        $pdf->Cell($w[2],4,utf8_decode($data[$x*$q+2]),0,0,"L");
+        $pdf->Cell($w[3],4,utf8_decode($data[$x*$q+3]),0,0,"L");
         $pdf->Ln();
     }
 
+    global $filas;
     // Closing line
     //footer
-    $pdf->Cell(63,4,"Subtotal",1,0,"C");
-    $pdf->Cell(11,4,$subtotal,1,0,"C");
     $pdf->Ln();
-    $pdf->Cell(array_sum($w),0,'','T',1);
+    $pdf->setFont("Courier","B",11);
+    $pdf->Cell(63,4,"Subtotal","T",0,"C");
+    $pdf->Cell(11,4,$subtotal,"T",0,"L");
+    $pdf->Ln();
+    $pdf->Cell(array_sum($w),0,'',0,1);
+    $pdf->setFont("Courier",null,9);
 
     if($_GET["efectivo"]==0) $pdf->Cell(40,4,"Tar",0);
     else $pdf->Cell(40,4,"Ef",0);
@@ -41,22 +47,33 @@ function ImprovedTable($pdf,$header,$data,$subtotal)
 
 
 //$pdf = new FPDF('P','mm','A4');
-$pdf = new FPDF('P','mm',array(80,80));  //ancho y alto
-$pdf->SetMargins(3,3,3); //LEFT, TOP RIGHT
+$values = explode(",", $_GET["orden"]);
+$filas=80+4*((sizeof($values)/4)-7);
+if($filas<80) $filas=80;
+$pdf = new FPDF('P','mm',array(80,$filas));  //ancho y alto ALTO SIEMPRE MAYO AL ANCHO
+//$pdf->SetMargins(3,3,3,3); //LEFT, TOP RIGHT
 $pdf->AddPage();
+$pdf->SetMargins(3, 3, 3); 
+$pdf->SetAutoPageBreak(false, 3);
 
 $pdf->setFont("Courier","B",11);   //se establece el tipo de letra
-
-$pdf->Cell(0,5,utf8_decode("Pizzería Mangiare"),"LBRT",1,"C"); //imprime una celda de borse rectangular
-$pdf->Cell(0,5,"Orden # xxxx",0,1,"C"); //imprime una celda de borse rectangular
+$pdf->Cell(0,5,utf8_decode("Pizzería Amangiare"),"LBRT",1,"C"); //imprime una celda de borse rectangular
 
 $pdf->setFont("Courier",null,9);
+$pdf->Cell(20,4,utf8_decode("Dirección:"),0);
+$pdf->Cell(43,4,"Conocoto, ....",0,1);
+$pdf->Cell(20,4,"Fecha    :",0);
+$pdf->Cell(43,4,$_GET["date"],0,1);
+$pdf->Cell(20,4,"Telf     : ",0);
+$pdf->Cell(43,4,"02-3442096",0,1);
 
-$pdf->Cell(43,5,$_GET["date"],0);
-$pdf->Cell(26,5,"Telf: 02-3442096",0);
-$pdf->Ln();
 
-$header = array('C', 'Descripción', 'Valor', 'Total');
+$pdf->setFont("Courier","B",11);   //se establece el tipo de letra
+$pdf->Cell(0,5,"Orden # xxxx",0,1,"C"); //imprime una celda de borse rectangular
+
+
+$header = array('C', 'Descripción', 'Val', 'Tot');
+
 
 /*
 $orden = array
@@ -67,7 +84,7 @@ $orden = array
   array(1,utf8_decode("Pizza mediana piña"),17,17)
 );*/
 
-$values = explode(",", $_GET["orden"]);
+
 ImprovedTable($pdf,$header,$values,$_GET["subtotal"]);
 
 //$pdf->Output("Reportes_PDF/notaVenta_".$_GET["user"].$_GET["date1"].".pdf","F");
