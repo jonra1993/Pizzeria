b83cad75b4f1e9b4d10d53465c8d94d8a50514cc
Jona <jon_ra@hotmail.es>
2018-08-23 13:36:42 -0500

    efectivo tarjera

--- realizar_venta.php
+++ realizar_venta.php
@@ -289,6 +289,20 @@
           </tfoot>
         </table>
         <button id="final_compra" type="button" class="btn btn-danger btn-block" onclick="f_final_compra()"style="display:none;">Finalizar Compra</button>
+        <div class=class="col-md-3">
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+              Pago en Efectivo
+            </label>
+          </div>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+              Pago con tarjeta
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   </div>
