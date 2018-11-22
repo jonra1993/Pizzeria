82b6ce46d206c2dc91c49d717db2c6b3dafb27bc
Jona <jon_ra@hotmail.es>
2018-11-22 17:44:07 -0500

    cote

--- final_compra_vuelto.php
+++ final_compra_vuelto.php
@@ -235,7 +235,7 @@ $page_title = 'Resumen de venta';
 
   //Cargar productos a inventario aproxiado
   <?php foreach ($products as $prod) :?>
-    $.ajax({url: DOMAIN+"guardar_invent_aprox.php?p_producto="+'<?php echo remove_junk($tempo); ?>'+"&p_cantidad="+val_aprox_<?php echo remove_junk($tempo); ?>});
+    $.ajax({url: DOMAIN+"guardar_invent_aprox.php?p_producto="+'<?php echo remove_junk($prod["name"]); ?>'+"&p_cantidad="+val_aprox_<?php echo remove_junk($prod["name"]); ?>});
   <?php endforeach; ?>
   //--------------------------------------------------------------------------------------
   //pdf_prueb();
